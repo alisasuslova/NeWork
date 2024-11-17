9f8146ee62de63bfdec35df935e933243bab6bad
alica_suslova <alisasuslova1408@gmail.com>
2024-11-17 19:24:54 +0300

    16.11.24 add fragments

---
+++ app/src/main/java/ru/netology/nework/presentation/PostsFragment.kt
@@ -0,0 +1,22 @@
+package ru.netology.nework.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.netology.nework.R
+
+class PostsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false)
+    }
+
+
+}
